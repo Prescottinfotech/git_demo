7ac378129ca99c37c067e9ae526b1d676f9bc6c9
Zeel Pandya <zeel1998@gmail.com>
2022-08-25 16:39:42 +0530

    login.js updated

---
+++ src/Login.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import './login.css'
+
+import Signup from "./Signup";
+import {Outlet,link, Link} from 'react-router-dom'
+
+function Login(){
+    return(
+        <><ul>  
+        <li><Link to='/'>Home</Link></li>  
+        
+        <li><Link to='/Login'>Login</Link></li>  
+        <li><Link to='/Signup'>Signup</Link></li>  
+      </ul>  
+      <Outlet/>
+        
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Login Form Demo</title>
+  </head>
+  <body>
+    
+    <div class="login-wrapper">
+      <form action="" class="form">
+        <h2>Login</h2>
+        <div class="input-group">
+          <input type="text" name="loginUser" id="loginUser" required />
+          <label for="loginUser">User Name</label>
+        </div>
+        <div class="input-group">
+          <input
+            type="password"
+            name="loginPassword"
+            id="loginPassword"
+            required
+          />
+          <label for="loginPassword">Password</label>
+        </div>
+        <input type="submit" value="Login" class="submit-btn" />
+      </form>
+    </div>
+  </body>
+</html>
+
+
+       </>
+    )
+}
+
+export default Login
