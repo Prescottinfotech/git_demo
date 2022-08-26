8b3ad23b42ce31e39b15a9d51da8974eff456ea2
Amee Savsani <amisavsani229@gmail.com>
2022-08-26 16:20:05 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react"
 import './App.css'
 import Login from './Login'
 
-function App(){
-    return(
-        <>
-        <Login/>
-
-
-       </>    )
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
 }
 
 export default App
