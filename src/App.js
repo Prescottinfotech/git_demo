659328f473cf23521d6b1d9f14953c8f79547feb
Amee Savsani <amisavsani229@gmail.com>
2022-08-26 16:22:48 +0530

    csdv

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import './App.css'
-import Registration from "./registration";
-function App(){
-  return(
-  <><Registration/></>
-  
+
+function App() {
+  return (
+    <>
+
+    </>
+
   )
 }
 
